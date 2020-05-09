74065a78eaa244ba5d7491d76e8e1936ca523232
mcmatthevan <mcmatthevan@gmail.com>
2020-05-09 13:39:57 +0200

    update maj script

--- scripts/majscript.py
+++ scripts/majscript.py
@@ -4,3 +4,5 @@ if os.system("git pull"):
     print("Une erreur est survenue pendant la mise à jour des fichiers.")
 else:
     print("La mise à jour des fichiers a été effectuée avec succès")
+
+input("Appuyez sur Entrée pour sortir...")
